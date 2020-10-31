10d3aed74a6d020dca49f991bf771b6c9e7c482b
Arjun Deshwal <khushideshwal334@gmail.com>
2020-10-31 09:51:39 +0530

    code added

---
+++ square.py
@@ -0,0 +1,8 @@
+print('Hello! I am a simple programme, I will ask you the square of numbers, if you will correct answers then one point will be added to your score.\n')
+
+print('Now tell me from where till where you want me to ask the squares.')
+
+from=int(input("From:"))
+till=int(input("Till:"))
+
+lis=[i for i in range 
