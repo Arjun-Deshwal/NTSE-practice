f062d5a8153995ed25be37f0e4b222c005618ca6
Arjun-Deshwal <67216579+Arjun-Deshwal@users.noreply.github.com>
2020-11-01 15:52:46 +0530

    Update alphabetnumbering.py

--- alphabetnumbering.py
+++ alphabetnumbering.py
@@ -1,12 +1,15 @@
 # knowing the alphabet numbering is an important part of NTSE. It can solve many questions using it
 # This is a small programme just for this only
 import random
-print("This programme is just for the practice of alphabet numbering, I will ask 10 quesions, elif you give correct answer then one point will be added to your score.")
+
+print("This programme is just for the practice of alphabet numbering, I will ask quesions, if you give correct answer then one point will be added to your score.")
 
 lis=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+queslimit=int(input("How many quesitons do you want me to ask:"))
 score=0
 k=0
-while k<10:
+
+while k<queslimit:
 	alphabet=random.choice(lis)
 	ans=int(input(f'Tell the numbering of {alphabet}:'))
 
@@ -14,13 +17,14 @@ while k<10:
 		print('You are correct! One point added to your score!')
 		score+=1
 		k+=1
-		print(f'{10-k} chances left.\n')
-	elif ans!=int:
-		print('Please enter a valid number.')
-		continue
-	else:
-		print('Opps! It\'s wrong.')
-		k+=1
-		print(f'{10-k} chances left\n')	
+		print(f"Your score is {score}")
+		print(f'{queslimit-k} chances left.\n')
 
-print(f"You scored {score} out of 10.")
+	else:
+		print(f'Opps! It\'s wrong. It\'s {lis.index(alphabet)+1}')
+		k+=1
+		print(f"Your score is {score}")
+
+		print(f'{queslimit-k} chances left\n')	
+
+print(f"You scored {score} out of {queslimit}.")
